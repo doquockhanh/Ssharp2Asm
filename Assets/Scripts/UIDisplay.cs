cc517afc8e8da43488718afd7594d7abd484c85c
doquockhanh <khanhquocdo.qt@gmail.com>
2024-06-12 15:15:09 +0700

    Asm gd 1 va 2

---
+++ Assets/Scripts/UIDisplay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIDisplay : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] Slider healthSlider;
+    [SerializeField] Health playerHealth;
+
+    [Header("Score")]
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        healthSlider.maxValue = playerHealth.GetHealth();
+    }
+
+    void Update()
+    {
+        healthSlider.value = playerHealth.GetHealth();
+        scoreText.text = ScoreKeeper.Instance.GetScore().ToString("000000000");
+    }
+}
